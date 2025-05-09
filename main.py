36e6ddd6de90b6603a84799ef3d1d56cf3bc47dd
loreargio <lorenzo.arg04@gmail.com>
2025-05-09 17:28:24 +0200

    Added man database and command

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ import pathvalidate
 from utils.curses_app import *
 from utils.file_reader import *
 from utils.table_drawer import *
+from utils.mandb import *
 
 
 def main(screen):
+    command_list = ["whoami", "close", "table", "clear", "man"]
     screen = create_app()
     rows, cols = screen.getmaxyx()
     window = curses.newwin(rows-2, cols-3, 1, 1)
@@ -54,6 +56,19 @@ def main(screen):
                 window.addch(curses.ACS_LRCORNER)
                 window.addch(curses.ACS_ULCORNER)
                 window.addch(curses.ACS_URCORNER)
+            case "man":
+                if len(in_line) == 1:
+                    message(window, "Please insert the name of a command")
+                    continue
+                if len(in_line) > 2:
+                    message(window, "Cannot use more than 1 argument")
+                    continue
+                if in_line[1] not in command_list:
+                    message(window, "Please use a correct command name")
+                    continue
+                man = find_man(in_line[1])
+                for l in man:
+                    message(window, l)
             case "clear":
                 window.clear()
 
